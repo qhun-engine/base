9e2b6819ac0c46cbaf71d669f67ad6db8a843fe7
wartoshika <dev@qhun.de>
2018-12-04 18:35:36 +0100

    added an index file for multiple exports

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export * from "./async/rx";
+
+export * from "./constraint/ClassConstructor";
+export * from "./constraint/Serializable";
+export * from "./constraint/Singleton";
+
+export * from "./di/Inject";
+export * from "./di/Injectable";
+export * from "./di/InjectableOptions";
+export * from "./di/Injector";
+
+export * from "./error/ConstraintError";
+export * from "./error/EngineError";
+export * from "./error/InjectorError";
+
+export * from "./reflection/MetadataRegistryService";
+export * from "./reflection/ReflectionMetadata";
